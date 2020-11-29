4bd01695413af6d95ed118d166cebf0442f85665
Safwan Shaheer <fireborn1003@gmail.com>
2020-11-29 22:23:00 +0600

    FEAT: Added listings graphql query using fetch

--- client/src/components/Listings/Listings.tsx
+++ client/src/components/Listings/Listings.tsx
@@ -1,5 +1,32 @@
 import React from "react"
 
-export const Listings = () => {
-  return <h2>Tinyhouse listings</h2>
+import { server } from "../../utils"
+
+const LISTINGS = `
+  query Listings{
+    listings{
+      id
+      title
+      image
+      address
+      price
+      rating
+    }
+  }
+`;
+
+interface ListingProps {
+  title: string
+}
+
+export const Listings = ({ title }: ListingProps) => {
+  const fetchListings = async () => {
+    const listings = await server.fetch({ query: LISTINGS });
+    console.log(listings);
+  }
+
+  return <div>
+    <h2>{title}</h2>
+    <button onClick={fetchListings}>Fetch listings</button>
+  </div>
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import { Listings } from "./components";
 
 ReactDOM.render(
-  <Listings />,
+  <Listings title="Tinyhouse Listings" />,
   document.getElementById('root')
 );
