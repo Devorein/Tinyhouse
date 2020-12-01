c436ddb203a7834e1c7a5f6bc7e2540cad3a72e5
Safwan Shaheer <fireborn1003@gmail.com>
2020-12-01 20:44:38 +0600

    FEAT: Added and handled error and loading state

--- client/src/components/Listings/Listings.tsx
+++ client/src/components/Listings/Listings.tsx
@@ -7,8 +7,7 @@ import { DeleteListingsData, DeleteListingsVariables, ListingProps, ListingsData
 
 export const Listings = ({ title }: ListingProps) => {
 
-  const { state, refetch } = useQuery<ListingsData>(LISTINGS)
-
+  const { state: { loading, data, error }, refetch } = useQuery<ListingsData>(LISTINGS)
   const deleteListing = async (id: string) => {
     await server.fetch<DeleteListingsData, DeleteListingsVariables>({
       query: DELETE_LISTING,
@@ -21,7 +20,7 @@ export const Listings = ({ title }: ListingProps) => {
 
   return <div>
     <h1>{title}</h1>
-    {state?.data ? state.data.listings.map(listing => {
+    {error ? <div>An unexpected error occurred</div> : loading ? <div>Loading</div> : data ? data.listings.map(listing => {
       return <div id={listing.id}>
         <h2>{listing.title}</h2>
         <button onClick={deleteListing.bind(null, listing.id)}>Delete listings</button>
