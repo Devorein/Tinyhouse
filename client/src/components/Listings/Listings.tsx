d90dbae98f26c50a59921d9388950fd0534e09b1
Safwan Shaheer <fireborn1003@gmail.com>
2020-12-04 00:43:34 +0600

    FEAT: Integrated generated types with hooks

--- client/src/components/Listings/Listings.tsx
+++ client/src/components/Listings/Listings.tsx
@@ -2,12 +2,13 @@ import React from "react"
 import { useMutation, useQuery } from "@apollo/client";
 
 import { LISTINGS, DELETE_LISTING } from "./graphql";
-import { DeleteListingsData, DeleteListingsVariables, ListingProps, ListingsData } from "./types";
+import { DeleteListing as DeleteListingsData, DeleteListingVariables } from "./__generated__/DeleteListing";
+import { Listings as ListingsData } from "./__generated__/Listings";
 
-export const Listings = ({ title }: ListingProps) => {
+export const Listings = ({ title }: { title: string }) => {
 
   const { loading, data, error, refetch } = useQuery<ListingsData>(LISTINGS)
-  const [deleteListing, { loading: deleteListingLoading, error: deleteListingError }] = useMutation<DeleteListingsData, DeleteListingsVariables>(DELETE_LISTING);
+  const [deleteListing, { loading: deleteListingLoading, error: deleteListingError }] = useMutation<DeleteListingsData, DeleteListingVariables>(DELETE_LISTING);
 
   return <div>
     <h1>{title}</h1>
