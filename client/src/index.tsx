6d03b7369269841e5c7cd08bfe754dd1915ed62a
Safwan Shaheer <fireborn1003@gmail.com>
2020-12-03 23:54:08 +0600

    FEAT: Added apollo-client deps

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 import { Listings } from "./components";
 
+const client = new ApolloClient({
+  uri: '/api',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <Listings title="Tinyhouse Listings" />,
+  <ApolloProvider client={client}>
+    <Listings title="Tinyhouse Listings" />
+  </ApolloProvider>,
   document.getElementById('root')
 );
