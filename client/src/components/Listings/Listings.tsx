6b48d938b0fc575e5ac651183ca3ad51b6ef9a70
Safwan Shaheer <fireborn1003@gmail.com>
2020-12-04 22:51:12 +0600

    FEAT: Added skeleton component for pre loading

--- client/src/components/Listings/Listings.tsx
+++ client/src/components/Listings/Listings.tsx
@@ -1,30 +1,32 @@
 import React from "react"
 import { useMutation, useQuery } from "@apollo/client";
-import List from "antd/es/list";
+import { Button, Spin, List, Avatar } from "antd";
 
 import { LISTINGS, DELETE_LISTING } from "./graphql";
 import { DeleteListing as DeleteListingsData, DeleteListingVariables } from "./__generated__/DeleteListing";
 import { Listings as ListingsData } from "./__generated__/Listings";
+import { ListingsSkeleton } from "./ListingsSkeleton";
+
+import "./index.css";
 
 export const Listings = ({ title }: { title: string }) => {
 
   const { loading, data, error, refetch } = useQuery<ListingsData>(LISTINGS)
   const [deleteListing, { loading: deleteListingLoading, error: deleteListingError }] = useMutation<DeleteListingsData, DeleteListingVariables>(DELETE_LISTING);
 
-  const listingsList = data ? <List itemLayout="horizontal" dataSource={data.listings} renderItem={(listing) => (
-    <List.Item>
-      <List.Item.Meta>{listing.title}</List.Item.Meta>
-      <button onClick={async () => {
-        await deleteListing({ variables: { id: listing.id } })
-        refetch();
-      }}>Delete listings</button>
+  const ListingsList = data ? <div><h1>{title}</h1><List itemLayout="horizontal" dataSource={data.listings} renderItem={(listing) => (
+    <List.Item actions={[<Button type="primary" onClick={async () => {
+      await deleteListing({ variables: { id: listing.id } })
+      refetch();
+    }}>Delete</Button>]}>
+      <List.Item.Meta title={listing.title} description={listing.address} avatar={<Avatar src={listing.image} shape="square" size={48} />} />
     </List.Item>
-  )} /> : <div>No listings available</div>;
+  )} /></div> : <div>No listings available</div>;
 
-  return <div>
-    <h1>{title}</h1>
-    {deleteListingLoading ? <div>Deletion in progress</div> : null}
-    {deleteListingError ? <div>Error Deleting listing</div> : null}
-    {error ? <div>An unexpected error occurred</div> : loading ? <div>Loading</div> : listingsList}
+  return <div className="listings">
+    <Spin spinning={deleteListingLoading}>
+      {deleteListingError ? <div>Error Deleting listing</div> : null}
+      {error ? <div>An unexpected error occurred</div> : loading ? <div className="listings"><ListingsSkeleton title={title} /></div> : ListingsList}
+    </Spin>
   </div>
 }
