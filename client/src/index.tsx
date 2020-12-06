a2481427c7fd4b00cf400fca7ee2d1cf29250250
Safwan Shaheer <fireborn1003@gmail.com>
2020-12-07 00:55:57 +0600

    FEAT: Basic routing setup for client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 import "./styles/index.css"
 
-import { Listings } from "./components";
+import { Home, Host, Listing, NotFound, User, Listings } from "./components";
 
 const client = new ApolloClient({
   uri: '/api',
   cache: new InMemoryCache()
 });
 
+const App = () => {
+  return <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/host" component={Host} />
+      <Route exact path="/listing:id" component={Listing} />
+      <Route exact path="/listings/:location?" component={Listings} />
+      <Route exact path="/user/:id" component={User} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
+}
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Listings title="Tinyhouse Listings" />
+    <App />
   </ApolloProvider>,
   document.getElementById('root')
 );
