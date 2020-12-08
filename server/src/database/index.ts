8362c34c0362b49b1d4addb64e213be6ed90448a
Safwan Shaheer <fireborn1003@gmail.com>
2020-12-09 01:30:07 +0600

    FEAT: Added interfaces for mongodb collections

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -3,7 +3,7 @@ import { Database, Listing, User, Booking } from "../lib/types";
 
 const { DB_USER, DB_USER_PASSWORD, DB_CLUSTER } = process.env;
 
-const url = `mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@${DB_CLUSTER}.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@${DB_CLUSTER}.mongodb.net/tinyhouse?retryWrites=true&w=majority`;
 
 export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
