a6af3bfd9cf31a66268ae9e2f1098081e410cfd6
Safwan Shaheer <fireborn1003@gmail.com>
2020-12-11 15:49:54 +0600

    FEAT: Added Login react component

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { Layout } from "antd";
 
 import "./styles/index.css"
 
-import { Home, Host, Listing, NotFound, User, Listings } from "./components";
+import { Home, Host, Listing, NotFound, User, Login, Listings } from "./components";
 
 const client = new ApolloClient({
   uri: '/api',
@@ -14,14 +15,17 @@ const client = new ApolloClient({
 
 const App = () => {
   return <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/host" component={Host} />
-      <Route exact path="/listing:id" component={Listing} />
-      <Route exact path="/listings/:location?" component={Listings} />
-      <Route exact path="/user/:id" component={User} />
-      <Route component={NotFound} />
-    </Switch>
+    <Layout id="app">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listing:id" component={Listing} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route exact path="/user/:id" component={User} />
+        <Route exact path="/login" component={Login} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
   </Router>
 }
 
