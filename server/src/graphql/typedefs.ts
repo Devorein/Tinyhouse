cdb0a237b1fa83360b10abfa0be5589e60fc1d3b
Safwan Shaheer <fireborn1003@gmail.com>
2020-12-19 20:24:23 +0600

    FEAT: Added User resolver and typedefs

--- server/src/graphql/typedefs.ts
+++ server/src/graphql/typedefs.ts
@@ -1,13 +1,13 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
-  
   input LogInInput{
     code: String!
   }
 
   type Query{
     authUrl: String!
+    user: String!
   }
 
   type Mutation{
@@ -15,11 +15,32 @@ export const typeDefs = gql`
     logOut: Viewer!
   }
 
+  type Bookings{
+    total: Int!
+    result: [Booking!]!
+  }
+
+  type Listings{
+    total: Int!
+    result: [Listing!]!
+  }
+  
   type Viewer{
     id: ID
     token: String
     avatar: String
     hasWallet: Boolean
     didRequest: Boolean!
+  }
+
+  type User{
+    id: String!
+    name: String!
+    avatar: String!
+    contact: String!
+    income: Int
+    bookings(limit: Int!, page: Int!): Bookings
+    listings(limit: Int!, page: Int!): Listings!
+    hasWallet: Boolean!
   }
 `
