2e885a4ef027358b04262e6be89b925eae86f8ab
Safwan Shaheer <fireborn1003@gmail.com>
2020-12-22 19:42:27 +0600

    FEAT: Added Query.user resolver

--- server/src/graphql/resolvers/User/index.ts
+++ server/src/graphql/resolvers/User/index.ts
@@ -1,7 +1,18 @@
 import { IResolvers } from "apollo-server-express";
 
+import { Context } from "../../../lib/types";
+import { UserArgs } from "./types";
+
 export const userResolvers: IResolvers = {
   Query: {
-    user: () => 'Query.user'
+    user: async (_root: undefined, { id }: UserArgs, { db }: Context,) => {
+      try {
+        const user = await db.users.findOne({ _id: id });
+        if (!user) throw new Error('User not found');
+        return user;
+      } catch (err) {
+        throw new Error(`User can't be queried. ${err.message}`)
+      }
+    }
   }
 }
